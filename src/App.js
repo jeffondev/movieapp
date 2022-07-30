c0f98966562481a337c228acc59724b72f7c6088
jeffondev <jeffondev@gmail.com>
2022-07-30 21:23:35 +0900

    component,JSX,props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+function Food({fav}) {
+  return <h1>I like {fav}</h1>;
+}
+
 function App() {
-  return <div>안녕하세요?</div>;
+  return (
+  <div>
+    <h1>안녕하세요?</h1>
+    <Food fav="kimchi" />
+    <Food fav="삽겹살" />
+    <Food fav="쭈꾸미" />
+  </div>
+  );  
 }
 
 export default App;
