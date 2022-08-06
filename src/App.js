a63258718eb24787216d56770b887446b8525fa6
jeffondev <jeffondev@gmail.com>
2022-08-06 15:13:22 +0900

    영화앱 만들기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
+import './App.css';
 
 class App extends React.Component {
   state ={
@@ -11,15 +13,36 @@ class App extends React.Component {
        data: {
          data: {movies},
        },
-      } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    console.log(movies);
+      } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading: false});
   };
   componentDidMount() {
     this.getMovies();
   }   
   render() {
-    const {isLoading} = this.state;
-    return <div>{isLoading ? 'Loading...' : 'we are ready'}</div>
+    const {isLoading, movies} = this.state;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loder">
+            <span class="loder__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies"> 
+            {movies.map((movie) => {
+              return <Movie 
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title} 
+                summary={movie.summary}
+                poster={movie.medium_cover_image}   
+              />
+            })}
+          </div>
+        )}
+       </section>
+    );  
   }  
 }
 export default App;
